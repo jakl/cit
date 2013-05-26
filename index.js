587635b9534d594767b3c19759559098840414bd
James Koval <james.ross.koval@gmail.com>
2013-05-25 23:04:38 -0700

    auto update

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-var params = {
-  book: "bible",
-  citation: "Gen. 1:1"
-};
+$(function(){
+  function Citation(citation){
+    this.book = parseInt(text.charAt(0)) ? 'science_health' : 'bible';
+    this.citation = citation;
 
-url = "http://cskit-server.herokuapp.com/v1/lesson/text.json?callback=?&";
-url += "payload=[%7B%22section%22:1,%22readings%22:%7B%22{{book}}%22:[%22{{citation}}%22]%7D%7D]";
+    url = "http://cskit-server.herokuapp.com/v1/lesson/text.json?callback=?&"
+    + "payload=[%7B%22section%22:1,%22readings%22:"
+    + "%7B%22{{book}}%22:[%22{{citation}}%22]%7D%7D]";
 
-//Use mustache here once it works...
-url = url.replace('{{book}}', params.book);
-url = url.replace('{{citation}}', params.citation);
+    url = url.replace('{{book}}', citation.book);
+    url = url.replace('{{citation}}', citation.citation);
 
-$.getJSON(url).done(function(data){
-  citation = data[1][params.book]
-  console.log(citation);
-  $('#citations').text(citation);
+    $.ajaxSetup({ async: false });
+    this.text = $.getJSON(url);
+    $.ajaxSetup({ async: true });
+  }
+
+  $('#citations').change(function(){
+    citation = new Citation($('#citations').text());
+    lookup_citation(citation,
+      function(text){
+        citation = data[1][citation.book]
+        console.log(citation.text);
+        $('#text').text(citation.text);
+      }
+    );
+  });
 });
