d9fb2f50e53bceeac7ef8c4f2f7b3ef53ad41853
James Koval <James.Ross.Koval@gmail.com>
2013-05-25 21:23:38 -0700

    gist commit 2

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var params = {
   citation: "Gen. 1:1"
 };
 
-url_template = "http://cskit-server.herokuapp.com/v1/lesson/text.json?callback=?&";
-url_template += "payload=[%7B%22section%22:1,%22readings%22:%7B%22{{book}}%22:[%22{{citation}}%22]%7D%7D]";
-url = Mustache.render(url_template, params);
+url = "http://cskit-server.herokuapp.com/v1/lesson/text.json?callback=?&";
+url += "payload=[%7B%22section%22:1,%22readings%22:%7B%22{{book}}%22:[%22{{citation}}%22]%7D%7D]";
+
+//Use mustache here once it works...
+url = url.replace('{{book}}', params.book);
+url.replace('{{citation}}', params.citation);
 
 $.getJSON(url).done(function(data){
   console.log(data);
